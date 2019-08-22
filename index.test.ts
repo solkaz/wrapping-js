c3f782db88af1f8d4b6bf476cf1e0859eb44f214
Jeff Held <jh.software.developer@gmail.com>
2019-08-22 19:18:49 +0200

    Initial implementation

--- index.test.ts
+++ index.test.ts
@@ -1,15 +1,68 @@
-import { add } from "./index";
+import {
+  add,
+  getDefaultBits,
+  setDefaultBits,
+  subtract,
+  multiply,
+} from "./index";
 
 describe("wrapping", () => {
+  describe("getDefaultBits/setDefaultBits", () => {
+    afterEach(() => {
+      // Set the default back to normal
+      setDefaultBits();
+    });
+
+    test("default bits", () => {
+      expect(getDefaultBits()).toBe(8);
+    });
+    test("setDefaultBits modifies default bits", () => {
+      setDefaultBits(2);
+      expect(getDefaultBits()).toBe(2);
+      expect(add(1, 3)).toBe(0);
+      expect(subtract(1, 3)).toBe(2);
+      expect(multiply(2, 2)).toBe(0);
+    });
+  });
+
   describe("add", () => {
-    test("handles cases that don't wrap", () => {
+    test("cases that don't wrap", () => {
       expect(add(0, 0)).toBe(0);
-      expect(add(0, 1)).toBe(1);
+      expect(add(1, 0)).toBe(1);
+      expect(add(1, 1)).toBe(2);
       expect(add(add(0, 1), 2)).toBe(3);
     });
 
-    test("handles cases that do wrap", () => {
+    test("cases that do wrap", () => {
       expect(add(1, 255)).toBe(0);
+      expect(add(255, 255)).toBe(254);
+    });
+  });
+
+  describe("subtract", () => {
+    test("cases that don't wrap", () => {
+      expect(subtract(0, 0)).toBe(0);
+      expect(subtract(1, 0)).toBe(1);
+      expect(subtract(1, 1)).toBe(0);
+    });
+
+    test("cases that do wrap", () => {
+      expect(subtract(0, 1)).toBe(255);
+      expect(subtract(1, 255)).toBe(2);
+    });
+  });
+
+  describe("multiply", () => {
+    test("cases that don't wrap", () => {
+      expect(multiply(0, 0)).toBe(0);
+      expect(multiply(1, 0)).toBe(0);
+      expect(multiply(1, 1)).toBe(1);
+      expect(multiply(3, 2)).toBe(6);
+    });
+
+    test("cases that do wrap", () => {
+      expect(multiply(128, 2)).toBe(0);
+      expect(multiply(25, 12)).toBe(44);
     });
   });
 });
--- index.ts
+++ index.ts
@@ -1,37 +1,74 @@
-let defaultBase = 8;
+/**
+ * The default bits value to use for an operation (add/subtract/multiply).
+ */
+let defaultBits: number = 8;
 
-function wrapNumber(n: number, base: number): number {
-  const max = 2 ** base;
+/**
+ * Gets the default bits used for an operation.
+ */
+export function getDefaultBits(): number {
+  return defaultBits;
+}
+
+/**
+ * Sets the default bits used for an operation.
+ */
+export function setDefaultBits(bits: number = 8) {
+  defaultBits = bits;
+}
+
+/**
+ * Wraps `n` according to the number of bits are allowed.
+ */
+function wrapNumber(n: number, bits: number): number {
+  const max = 2 ** bits;
   if (n < 0) {
     return max + n;
   }
   return n % max;
 }
 
+/**
+ * Adds `lhs` to `rhs`, returning the wrapped value.
+ *
+ * @param lhs Left-hand operand of the operation.
+ * @param rhs Right-hand operand of the operation.
+ * @param bits Number of bits allowed. This will default to `defaultBits`.
+ */
 export function add(
-  lhs: number = 0,
-  rhs: number = 0,
-  base = defaultBase
+  lhs: number,
+  rhs: number,
+  bits: number = defaultBits
 ): number {
-  return wrapNumber(lhs + rhs, base);
+  return wrapNumber(lhs + rhs, bits);
 }
 
+/**
+ * Subtracts `rhs` from `lhs`, returning the wrapped version.
+ *
+ * @param lhs Left-hand operand of the operation.
+ * @param rhs Right-hand operand of the operation.
+ * @param bits Number of bits allowed. This will default to `defaultBits`.
+ */
 export function subtract(
-  lhs: number = 0,
-  rhs: number = 0,
-  base = defaultBase
+  lhs: number,
+  rhs: number,
+  bits: number = defaultBits
 ): number {
-  return wrapNumber(lhs - rhs, base);
+  return wrapNumber(lhs - rhs, bits);
 }
 
+/**
+ * Multiplies `lhs` by `rhs`, returning the wrapped version.
+ *
+ * @param lhs Left-hand operand of the operation.
+ * @param rhs Right-hand operand of the operation.
+ * @param bits Number of bits allowed. This will default to `defaultBits`.
+ */
 export function multiply(
-  lhs: number = 0,
-  rhs: number = 0,
-  base = defaultBase
+  lhs: number,
+  rhs: number,
+  bits: number = defaultBits
 ): number {
-  return wrapNumber(lhs * rhs, base);
-}
-
-export function setDefaultBase(base: number = 8) {
-  defaultBase = base;
+  return wrapNumber(lhs * rhs, bits);
 }
